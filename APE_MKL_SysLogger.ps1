0caed2fb8486557f24c4a2752d7b8b029ca130ad
Nomade <102302877+TheArabicMonster@users.noreply.github.com>
2026-01-26 10:50:22 +0100

    ajout de toutes les commandes

--- APE_MKL_SysLogger.ps1
+++ APE_MKL_SysLogger.ps1
@@ -47,7 +47,7 @@ param($FilePath, $EngineName)
 
 ###################################################################################################################
 # Zone de définition des variables et fonctions, avec exemples
-
+$date = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
 ###################################################################################################################
 # Zone de tests comme les paramètres renseignés ou les droits administrateurs
 
@@ -63,7 +63,18 @@ if(!$FilePath)
 
 # Ce que fait le script, ici, afficher un message
 Write-Host "coucou"
-    
+try {
+    (Get-CimInstance Win32_ComputerSystem).Name,
+    (Get-CimInstance Win32_OperatingSystem).Version,
+    (Get-CimInstance Win32_LogicalDisk | Select-Object DeviceID, @{Name="Taille";Expression={[math]::round($_.Size / 1GB)}},  @{Name="EspaceLibre";Expression={[math]::round($_.FreeSpace / 1GB)}}),
+    (Get-CimInstance Win32_OperatingSystem | Select-Object @{Name="RAM_Totale";Expression={[math]::round($_.TotalVisibleMemorySize / 1MB)}}, @{Name="RAM_Libre";Expression={[math]::round($_.FreePhysicalMemory / 1MB)}}),
+    (Get-CimInstance Win32_InstalledWin32Program).Name,
+    (Get-CimInstance Win32_OperatingSystem | Select-Object @{Name="Uptime";Expression={((Get-Date) - $_.LastBootUpTime).ToString("hh\:mm\:ss")}})
+
+}
+catch {
+    <#Do this if a terminating exception happens#>
+}
 
 
 
